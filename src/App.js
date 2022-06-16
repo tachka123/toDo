43a44b64eb5227c77d504d76c93cb420b8ed88fe
Alexander Savchuk <123tachka123@gmail.com>
2022-06-16 19:27:50 +0300

    Page wrapper with context theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Router from "./Router";
+import { ThemeContext } from "./Context";
+import "./App.css";
+import PageWrapper from "./components/PageWrapper";
+import { useContext } from "react";
 
 function App() {
+  const theme = useContext(ThemeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper theme={theme.theme}>
+      <Router />
+    </PageWrapper>
   );
 }
 
